846b419e3f824bb9d40e458cabe5a89ec25cbbfb
Taewoong Heo <chamjoeun0111@gmail.com>
2026-04-27 01:15:37 +0900

    add: parallaz-animation

--- app/interactions/parallax-animation/index.tsx
+++ app/interactions/parallax-animation/index.tsx
@@ -1,15 +1,27 @@
 import {
   Dimensions,
-  Image,
-  ScrollView,
   StyleSheet,
   View,
   ViewStyle,
   StyleProp,
 } from "react-native";
-import Animated from "react-native-reanimated";
+import Animated, {
+  interpolate,
+  SharedValue,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useScrollOffset,
+} from "react-native-reanimated";
 
 export const Images = [
+  "https://images.unsplash.com/photo-1712174863129-dcbd52938915?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1621897100070-055b183ead92?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1623093386041-a0915e5a1ca4?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1513883524931-aaab83bcb19b?q=100&w=2992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1712174863129-dcbd52938915?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1621897100070-055b183ead92?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1623093386041-a0915e5a1ca4?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1513883524931-aaab83bcb19b?q=100&w=2992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1712174863129-dcbd52938915?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1621897100070-055b183ead92?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1623093386041-a0915e5a1ca4?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -26,9 +38,14 @@ const ItemContainerWidth = ItemWidth + ItemInternalPadding * 2;
 const ListPadding = (WindowWidth - ItemContainerWidth) / 2;
 
 export default function ParallaxAnimation() {
+  const scrollRef = useAnimatedRef<Animated.ScrollView>();
+
+  const scrollOffset = useScrollOffset(scrollRef);
+
   return (
     <View style={styles.container}>
       <Animated.ScrollView
+        ref={scrollRef}
         horizontal
         style={{ flex: 1 }}
         contentContainerStyle={{
@@ -42,8 +59,10 @@ export default function ParallaxAnimation() {
       >
         {Images.map((image, index) => (
           <ListItem
+            scrollOffset={scrollOffset}
             image={image}
             key={index}
+            index={index}
             itemWidth={ItemWidth}
             style={{ marginHorizontal: ItemInternalPadding }}
           />
@@ -66,15 +85,56 @@ type ListItemProps = {
   image: string;
   itemWidth: number;
   style?: StyleProp<ViewStyle>;
+  scrollOffset: SharedValue<number>;
+  index: number;
 };
 
-function ListItem({ image, itemWidth, style }: ListItemProps) {
+function ListItem({
+  image,
+  itemWidth,
+  style,
+  scrollOffset,
+  index,
+}: ListItemProps) {
+  const inputRange = [
+    ItemContainerWidth * (index - 1),
+    ItemContainerWidth * index,
+    ItemContainerWidth * (index + 1),
+  ];
+
+  const rImageStyle = useAnimatedStyle(() => {
+    const translateX = interpolate(
+      scrollOffset.value,
+      inputRange,
+      [-200, 0, 200],
+    );
+
+    return {
+      transform: [{ translateX: translateX }, { scale: 1.7 }],
+    };
+  }, []);
+
+  const rContainerStyle = useAnimatedStyle(() => {
+    const outputRange = [1, 1.05, 1];
+    const scale = interpolate(scrollOffset.value, inputRange, outputRange);
+
+    return {
+      transform: [{ scale: scale }],
+    };
+  }, []);
+
   return (
-    <View style={[style]}>
-      <Image
+    <Animated.View
+      style={[{ overflow: "hidden", borderRadius: 20 }, style, rContainerStyle]}
+    >
+      <Animated.Image
         source={{ uri: image }}
-        style={{ width: itemWidth, aspectRatio: 0.6, borderRadius: 20 }}
+        resizeMode="cover"
+        style={[
+          { width: itemWidth, aspectRatio: 0.6, borderRadius: 20 },
+          rImageStyle,
+        ]}
       />
-    </View>
+    </Animated.View>
   );
 }
